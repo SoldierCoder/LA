5cb57a42e41823ab8ef727c6ae90d37120b5f9d1
Ed Drain <geekprogrammer.ed@gmail.com>
2014-09-13 07:46:26 -0400

    Initial commit

---
+++ hrs_per_year.rb
@@ -0,0 +1,10 @@
+hours_in_day = 24
+days_in_year = 365.25
+puts "hours in a year: #{hours_in_day * days_in_year}"
+
+minutes_in_hour = 60
+years_in_decade = 10
+
+minutes_in_decade = minutes_in_hour * hours_in_day * days_in_year * years_in_decade
+puts "minutes in decade: #{minutes_in_decade}"
+
